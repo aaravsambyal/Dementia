4d779ea545e13b817680b708fbce6304dfe40fe4
Aditya-Sajgotra <anukhajuria089@gmail.com>
2025-09-26 00:15:39 +0530

    Added tasks and main page

---
+++ tasks/reaction_task/shared/languages.js
@@ -0,0 +1,285 @@
+const en = {
+    welcomePage: {
+        welcome: "Welcome!",
+        clickNext: "Click on <strong>Next</strong> to continue!"
+    },
+    instruction: {
+        blueStar: "In this task, a grid with four different locations will appear on the screen. In each location, you will see a blue star.",
+        PorR: "In every few seconds, a <strong>P</strong> or <strong>R</strong> letter will appear in one of the four locations.",
+        pressSpace: "If you see a <strong>P</strong>, <strong>PRESS THE SPACEBAR</strong> as soon as you can.</p>",
+        dontPress: "If you see an <strong>R</strong>, <strong>DO NOT PRESS</strong> the spacebar.",
+        ready: "If you understand the instructions, click on <strong>Next</strong>!",
+        img: "../static/images/instruction_en.gif"
+    },
+    practice: "If you are ready, press any key for a short practice!",
+    task: {
+        begin: "Good job! The task is starting now. From now on, you won't receive feedback on your performance.",
+        ready: "If you are ready, press any key to start the task!",
+        pressP: "Press <strong>SPACE</strong> if you see a <strong>P</strong>!",
+        pressR: "Press <strong>SPACE</strong> if you see a <strong>R</strong>!"
+    },
+    end:  {
+        end: "End of the task.",
+        thankYou: "Thank you!",
+        answer1: "You answered correctly in ",
+        answer2: "% of the trials."
+    },
+    instructionChange:  {
+        attention: "Attention!",
+        change: "Now your task will change a little.",
+        pressSpace: "From now on, if you see an <strong>R</strong>, <strong>PRESS THE SPACEBAR</strong> as soon as you can.",
+        dontPress: "If you see an <strong>P</strong>, <strong>DO NOT PRESS</strong> the spacebar.",
+        img: "../static/images/instruction2_en.gif"
+    },
+    button: {
+        next: "Next",
+        previous: "Previous"
+    },
+    fullscreen: {
+        fullscreenText: "The experiment will switch to full screen mode when you press the button below.",
+        fullscreenButton: "Continue"
+    },
+    feedback: {
+        correct: "Correct!",
+        wrong: "Wrong!"
+    },
+    parameters: {
+        subject: "Subject Number:",
+        session: "Session Number:"
+    },
+    startWarning: {
+        startSubject: "You are about to start the task with<strong> Subject Number ",
+        startSession: "</strong> and <strong>Session Number ",
+        startButton: "Start the task!",
+        goBackButton: "Change the parameters"
+    },
+}
+
+
+const hu = {
+    welcomePage: {
+        welcome: "Üdvözlünk!",
+        clickNext: "Kattints a <strong>Tovább</strong> gombra a folytatáshoz!"
+    },
+    instruction: {
+        blueStar: "Ebben a feladatban egy négyzetrácsot fogsz látni a képernyőn négy pozícióval. A négy pozíció mindegyikén egy kék csillag fog megjelenni.",
+        PorR: "Néhány másodpercenként egy <strong>P</strong> vagy egy <strong>R</strong> betű fog megjelenni a négy pozíció valamelyikén.",
+        pressSpace: "Ha <strong>P</strong> betűt látsz, <strong>NYOMD MEG A SZÓKÖZ GOMBOT</strong> amilyen gyorsan csak tudod!</p>",
+        dontPress: "Ha  <strong>R</strong> betűt látsz, <strong>NE NYOMJ MEG SEMMILYEN GOMBOT</strong>.",
+        ready: "Ha érthetőek az instrukciók, kattints a <strong>Tovább</strong> gombra a folytatáshoz!",
+        img: "../static/images/instruction_hu.gif"
+    },
+    practice: "Ha készen állsz, nyomd meg bármelyik gombot egy rövid gyakorláshoz!",
+    task: {
+        begin: "Szép munka! A feladat hamarosan kezdődik. Mostantól nem fogsz visszajelzést kapni a teljesítményedről.",
+        ready: "Ha készen állsz, nyomd meg bármelyik gombot a kezdéshez!",
+        pressP: "Nyomd meg a <strong>SZÓKÖZT</strong>, ha <strong>P</strong> betűt látsz!",
+        pressR: "Nyomd meg a <strong>SZÓKÖZT</strong>, ha <strong>R</strong> betűt látsz!"
+    },
+    end:  {
+        end: "Vége a feladatnak.",
+        thankYou: "Köszönjük!",
+        answer1: "Az esetek ",
+        answer2: "%-ában válaszoltál helyesen."
+    },
+    instructionChange:  {
+        attention: "Figyelem!",
+        change: "Most egy kicsit megváltozik a feladatod.",
+        pressSpace: "Mostantól, ha <strong>R</strong> betűt látsz, akkor <strong>NYOMD MEG A SZÓKÖZ GOMBOT</strong> amilyen gyorsan csak tudod!",
+        dontPress: "Ha <strong>P</strong> betűt látsz, <strong>NE NYOMJ MEG SEMMILYEN GOMBOT</strong>.",
+        img: "../static/images/instruction2_hu.gif"
+    },
+    button: {
+        next: "Tovább",
+        previous: "Vissza"
+    },
+    fullscreen: {
+        fullscreenText: "A feladat teljes képernyőre ugrik, amint a Tovább gombra kattintasz.",
+        fullscreenButton: "Tovább"
+    },
+    feedback: {
+        correct: "Helyes!",
+        wrong: "Helytelen!"
+    },
+    parameters: {
+        subject: "Vizsgálati személy száma: ",
+        session: "Ülés száma: "
+    },
+    startWarning: {
+        startSubject: "Feladat kezdése <strong> ezzel a vizsgálati személy számmal: ",
+        startSession: "</strong> és <strong>ezzel az ülésszámmal: ",
+        startButton: "Feladat kezdése",
+        goBackButton: "Paraméterek változtatása"
+    },
+}
+
+const es = {
+    welcomePage: {
+        welcome: "Bienvenido@!",
+        clickNext: "Haz clic en <strong>Siguiente</strong> para continuar!"
+    },
+    instruction: {
+        blueStar: "En esta tarea, una cuadrícula con cuatro ubicaciones diferentes aparecerá en pantalla. En cada ubicación verás una estrella azul.",
+        PorR: "Durante un corto instante, una letra <strong>P</strong>, o una letra <strong>R</strong>, aparecerá en una de las ubicaciones.",
+        pressSpace: "Si ves la letra <strong>P</strong>, <strong>PRESIONA LA BARRA ESPACIADORA</strong> lo más rápido que puedas.</p>",
+        dontPress: "Si ves la letra <strong>R</strong>, <strong>NO PRESIONES</strong> la barra espaciadora.",
+        ready: "Si comprendiste las instrucciones, haz clic en <strong>Siguiente</strong>!",
+        img: "../static/images/instruction_en.gif"
+    },
+    practice: "Si estás listo, haz clic en cualquier tecla para empezar el entrenamiento!",
+    task: {
+        begin: "Bien hecho! La tarea empezará a continuación. A partir de ahora, no recibirás ningún comentario en relación a tus respuestas.",
+        ready: "Si estás listo, haz clic en cualquier tecla para empezar la tarea!",
+        pressP: "Haz clic en la tecla <strong>ESPACIO</strong> si ves una <strong>P</strong>!",
+        pressR: "Haz clic en la tecla <strong>ESPACIO</strong> si ves una <strong>R</strong>!"
+    },
+    end:  {
+        end: "Fin de la tarea.",
+        thankYou: "Gracias!",
+        answer1: "Respondiste correctamente en ",
+        answer2: "% de la prueba."
+    },
+    instructionChange:  {
+        attention: "Cuidado!",
+        change: "Ahora la tarea va a cambiar ligeramente.",
+        pressSpace: "A partir de ahora, si ves una <strong>R</strong>, <strong>PRESIONA LA BARRA ESPACIADORA</strong> lo más rápido que puedas.",
+        dontPress: "Si ves una <strong>P</strong>, <strong>NO PRESIONES</strong> la barra espaciadora.",
+        img: "../static/images/instruction2_en.gif"
+    },
+    button: {
+        next: "Siguiente",
+        previous: "Atrás"
+    },
+    fullscreen: {
+        fullscreenText: "El experimento pasará a pantalla completa al cliquear en Continuar.",
+        fullscreenButton: "Continuar"
+    },
+    feedback: {
+        correct: "Correcto!",
+        wrong: "Incorrecto!"
+    },
+    parameters: {
+        subject: "Número de asunto: ",
+        session: "Número de sesión: "
+    },
+    startWarning: {
+        startSubject: "Está a punto de iniciar la tarea con <strong>Número de Asunto ",
+        startSession: "</strong> y <strong>Número de Sesión ",
+        startButton: "¡Inicia la tarea!",
+        goBackButton: "Cambiar los parámetros"
+    },
+}
+
+const fr = {
+    welcomePage: {
+        welcome: "Bienvenue!",
+        clickNext: "Cliquez sur <strong>Suivant</strong> pour continuer!"
+    },
+    instruction: {
+        blueStar: "Dans cette tâche, une grille avec quatre emplacements différents apparaîtra à l'écran. Dans chaque emplacement, vous verrez une étoile bleue.",
+        PorR: "Chaque seconde, la lettre <strong>P</strong> ou <strong>R</strong> apparaîtra dans un des emplacements.",
+        pressSpace: "Si vous voyez un <strong>P</strong>, <strong>APPUYEZ SUR LA BARRE D'ESPACE</strong> aussi vite que vous le pouvez.</p>",
+        dontPress: "Si vous voyez un <strong>R</strong>, <strong>N'APPUYEZ PAS</strong> sur la barre d'espace.",
+        ready: "Si vous avez compris les instructions, cliquez sur <strong>SUIVANT</strong>!",
+        img: "../static/images/instruction_en.gif"
+    },
+    practice: "Si vous êtes prêt, appuyez sur n'importe quelle touche du clavier pour démarrer un petit entraînement!",
+    task: {
+        begin: "Bien joué! La tâche va maintenant démarrer. A partir de maintenant, vous ne recevrez plus de retour sur votre performance.",
+        ready: "Si vous êtes prêt, appuyez sur n'importe quelle touche de votre clavier pour démarrer la tâche!",
+        pressP: "Appuyez sur <strong>LA BARRE D'ESPACE</strong> si vous voyez un <strong>P</strong> !",
+        pressR: "Appuyez sur <strong>LA BARRE D'ESPACE</strong> si vous voyez un <strong>R</strong> !"
+    },
+    end:  {
+        end: "Fin de la tâche.",
+        thankYou: "Merci!",
+        answer1: "Vous avez répondu correctement à ",
+        answer2: "% des essais."
+    },
+    instructionChange:  {
+        attention: "Attention!",
+        change: "La tâche va changer un petit peu.",
+        pressSpace: "Maintenant, si vous voyez un <strong>R</strong>, <strong>APPUYEZ SUR LA BARRE D'ESPACE</strong> aussi vite que possible.",
+        dontPress: "Si vous voyez un <strong>P</strong>, <strong>N'APPUYEZ PAS</strong> sur la barre d'espace.",
+        img: "../static/images/instruction2_en.gif"
+    },
+    button: {
+        next: "Suivant",
+        previous: "Précédent"
+    },
+    fullscreen: {
+        fullscreenText: "L'expérience passera en mode plein écran lorsque vous appuierez sur le bouton ci-dessous.",
+        fullscreenButton: "Continuer"
+    },
+    feedback: {
+        correct: "Correct!",
+        wrong: "Faux!"
+    },
+    parameters: {
+        subject: "Numéro du sujet : ",
+        session: "Numéro de session : "
+    },
+    startWarning: {
+        startSubject: "Vous êtes sur le point de commencer la tâche avec <strong>Numéro de sujet ",
+        startSession: "</strong> et <strong>Numéro de session ",
+        startButton: "Commencez la tâche !",
+        goBackButton: "Modifier les paramètres"
+    },
+}
+
+const pt = {
+    welcomePage: {
+        welcome: "Bem-vindo!",
+        clickNext: "Clique em <strong>Próximo</strong> para continuar!"
+    },
+    instruction: {
+        blueStar: "Nessa tarefa, você verá na tela uma grade com quatro espaços diferentes. Em cada espaço, verá uma estrela azul.",
+        PorR: "Em intervalos de poucos segundos, uma letra <strong>P</strong> ou <strong>R</strong> vai aparecer em um dos quatro espaços.",
+        pressSpace: "Se você ver a letra <strong>P</strong>, <strong>APERTE A BARRA DE ESPAÇO</strong> o mais cedo que puder.</p>",
+        dontPress: "Se você ver a letra <strong>R</strong>, <strong>NÃO APERTE</strong> a barra de espaço.",
+        ready: "Se você entendeu as instruções, clique em <strong>Próximo</strong>!",
+        img: "../static/images/instruction_en.gif"
+    },
+    practice: "Se estiver pronto, aperte qualquer tecla para um pequeno treino!",
+    task: {
+        begin: "Bom trabalho! A tarefa começa agora. De agora em diante, você não receberá mais um feedback da sua performance.",
+        ready: "Se estiver pronto, aperte qualquer tecla para iniciar a tarefa!",
+        pressP: "Aperte <strong>ESPAÇO</strong> se você ver um <strong>P</strong>!",
+        pressR: "Aperte <strong>ESPAÇO</strong> se você ver um <strong>R</strong>!"
+    },
+    end:  {
+        end: "Fim da tarefa.",
+        thankYou: "Muito obrigado!",
+        answer1: "Você respondeu corretamente em ",
+        answer2: "% das tentativas."
+    },
+    instructionChange:  {
+        attention: "Atenção!",
+        change: "Agora sua tarefa vai mudar um pouco.",
+        pressSpace: "De agora em diante, se você ver a letra <strong>R</strong>, <strong>APERTE A BARRA DE ESPAÇO</strong> o mais cedo que puder..",
+        dontPress: "E se você ver a letra <strong>P</strong>, <strong>NÃO APERTE</strong> a barra de espaço.",
+        img: "../static/images/instruction2_en.gif"
+    },
+    button: {
+        next: "Próximo",
+        previous: "Anterior"
+    },
+    fullscreen: {
+        fullscreenText: "O experimento irá passar para o modo tela cheia quando você clicar no botão abaixo.",
+        fullscreenButton: "Continue"
+    },
+    feedback: {
+        correct: "Correto!",
+        wrong: "Errado!"
+    },
+    parameters: {
+        subject: "Assunto número: ",
+        session: "Número da sessão: "
+    },
+    startWarning: {
+        startSubject: "Você está prestes a iniciar a tarefa com<strong> Número do assuntot ",
+        startSession: "</strong> e <strong>Sessão número ",
+        startButton: "Comece a tarefa!",
+        goBackButton: "Alterar os parâmetros"
+    },
+}
